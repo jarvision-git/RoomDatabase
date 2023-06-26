d1fd8f19fd255c08bfd731d86f76b2d9815ece2f
Kushal <kushalldesai@gmail.com>
2023-06-26 17:07:13 +0300

    Initial commit, implemented room database

---
+++ app/src/main/java/com/example/roomdatabase/Word.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "word_table")
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "word") val word: String
+
+)
