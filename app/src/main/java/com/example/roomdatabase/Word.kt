82ac5b7ecc77aa8d0ea08a6ffab785c0f2ee5631
Kushal <kushalldesai@gmail.com>
2023-06-26 17:45:37 +0300

    working room database

--- app/src/main/java/com/example/roomdatabase/Word.kt
+++ app/src/main/java/com/example/roomdatabase/Word.kt
@@ -6,9 +6,4 @@ import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "word_table")
-class Word(
-
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "word") val word: String
-
-)
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
